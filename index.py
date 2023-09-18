e2487a54be3bf52b55fb57e7523629e77db80cb7
NilayYadav <nilayyadav10@gmail.com>
2023-09-18 15:01:57 +0530

    9934

--- index.py
+++ index.py
@@ -1,13 +1,38 @@
+from flask import Flask, request, jsonify
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
 
+app = Flask(__name__)
 
+# Load the processor and model
 processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
 model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
 
-inputs = processor(
-    text=["80s pop track with bassy drums and synth", "90s rock song with loud guitars and heavy drums"],
-    padding=True,
-    return_tensors="pt",
-)
+@app.route('/generate_music', methods=['POST'])
+def generate_music():
+    try:
+        # Get the input text from the request body
+        data = request.json
+        input_text = data.get('input_text')
 
-audio_values = model.generate(**inputs, max_new_tokens=256)
+        # Check if input_text is provided
+        if not input_text:
+            return jsonify({"error": "Input text is missing"}), 400
+
+        # Tokenize the input text
+        inputs = processor(
+            text=input_text,
+            padding=True,
+            return_tensors="pt",
+        )
+
+        # Generate music
+        audio_values = model.generate(**inputs, max_new_tokens=256)
+        
+        # Return the generated audio values
+        return jsonify({"audio_values"})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
